fbaee8b94419b41c4cf3a71757635580fb5ade9b
nalssee <kenjin@sdf.org>
2016-10-08 00:34:31 +0900

    ..

---
+++ tableC.py
@@ -0,0 +1,15 @@
+"""
+tableB turned out to be a complete failure
+So I want to test what's wrong with it.
+I am going to group them based on the scale of word ratios
+and try to analyse the cause of problems
+"""
+
+from pydwork.sqlplus import *
+
+
+set_workspace('space.db') as c:
+
+    pass
+
+
